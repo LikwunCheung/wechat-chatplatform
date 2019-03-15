375dc1de6d76a03bbb70e13d60d2ebd4c3a86966
zhanglihuan <lihuanz@student.unimelb.edu.au>
2019-03-15 12:28:07 +1100

    init app

---
+++ employee_group/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class EmployeeGroupConfig(AppConfig):
+    name = 'employee_group'
---
+++ platform_info/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PlatformInfoConfig(AppConfig):
+    name = 'platform_info'
