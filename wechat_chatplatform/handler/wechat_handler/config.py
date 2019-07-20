a34dea1eccbfb9fa58500b338c14f2bf99c5138e
James Wang <36892657+cn-wx@users.noreply.github.com>
2019-07-20 20:47:32 +1000

    Update config.py

--- wechat_chatplatform/handler/wechat_handler/config.py
+++ wechat_chatplatform/handler/wechat_handler/config.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
 
 
-# APP_ID = 'wx67e680017dca0b2b'
-# APP_SECRET = '3771aed06e46c0c8048ce4df2dc7bfba'
-APP_ID = 'wx1c5f306604b0728c'
-APP_SECRET = '9ac35f05b4aba5dc4b35b401a16291f5'
+APP_ID = 'wx67e680017dca0b2b'
+APP_SECRET = '3771aed06e46c0c8048ce4df2dc7bfba'
+# APP_ID = 'wx1c5f306604b0728c'
+# APP_SECRET = '9ac35f05b4aba5dc4b35b401a16291f5'
 
 ACCESS_TOKEN_URL = 'https://api.weixin.qq.com/cgi-bin/token'
 ACCESS_TOKEN_GRANT_TYPE = 'client_credential'
 
 USER_CODE_URL = 'https://open.weixin.qq.com/connect/oauth2/authorize'
-USER_CODE_REDIRECT_URL = 'http://www.suavechat.com/api/v1/wxoauth/recall/'
+USER_CODE_REDIRECT_URL = 'http://www.suavechat.top/api/v1/wxoauth/recall/'
 USER_CODE_RESPONSE_TYPE = 'code'
 USER_CODE_SCOPE = 'snsapi_userinfo'
 USER_CODE_WECHAT = '#wechat_redirect'
