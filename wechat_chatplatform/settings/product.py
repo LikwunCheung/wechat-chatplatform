f098a7d4fabab3e42c236eba9ee3ac4d5d09ada1
zhanglihuan <lihuanz@student.unimelb.edu.au>
2019-07-23 14:12:13 +1000

    debug: register 400

--- wechat_chatplatform/settings/product.py
+++ wechat_chatplatform/settings/product.py
@@ -9,8 +9,6 @@ DEBUG = True
 ALLOWED_HOSTS = ['47.74.66.156', 'www.suavechat.com']
 
 SESSION_ENGINE = 'django.contrib.sessions.backends.cached_db'
-SESSION_COOKIE_DOMAIN = '.suavechat.com'
-SESSION_COOKIE_NAME = 'suavesessionid'
 
 DATABASES = {
     # 'default': {
